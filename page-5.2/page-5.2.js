7759b612d315132e52c1ecebe8f640144fcb749a
Teacher-Jorge <79639664+Teacher-Jorge@users.noreply.github.com>
2025-08-05 21:13:52 -0300

    Add files via upload

--- page-5.2/page-5.2.js
+++ page-5.2/page-5.2.js
@@ -1,24 +1,24 @@
-const input1 = document.getElementById('input1')
-const input2 = document.getElementById('input2')
-const input3 = document.getElementById('input3')
-const input4 = document.getElementById('input4')
-const input5 = document.getElementById('input5')
-const input6 = document.getElementById('input6')
-const input7 = document.getElementById('input7')
-const input8 = document.getElementById('input8')
+const input1 = document.getElementById('input1');
+const input2 = document.getElementById('input2');
+const input3 = document.getElementById('input3');
+const input4 = document.getElementById('input4');
+const input5 = document.getElementById('input5');
+const input6 = document.getElementById('input6');
+const input7 = document.getElementById('input7');
+const input8 = document.getElementById('input8');
 
-const correct1 = document.getElementById('correct1')
-const correct2 = document.getElementById('correct2')
-const correct3 = document.getElementById('correct3')
-const correct4 = document.getElementById('correct4')
-const correct5 = document.getElementById('correct5')
-const correct6 = document.getElementById('correct6')
-const correct7 = document.getElementById('correct7')
-const correct8 = document.getElementById('correct8')
+const correct1 = document.getElementById('correct1');
+const correct2 = document.getElementById('correct2');
+const correct3 = document.getElementById('correct3');
+const correct4 = document.getElementById('correct4');
+const correct5 = document.getElementById('correct5');
+const correct6 = document.getElementById('correct6');
+const correct7 = document.getElementById('correct7');
+const correct8 = document.getElementById('correct8');
 
 addEventListener('keydown', (e) => {
     const audio = document.getElementById('click')
-    audio.play()
+    audio.play();
 })
 
 addEventListener('keyup', (e) => {
@@ -27,8 +27,7 @@ addEventListener('keyup', (e) => {
         input1.value = "Are"
         input1.style.color = 'darkgreen'
         input1.style.backgroundColor = 'yellow'
-        document.getElementById('input2').focus()
-        document.getElementById('click').play()
+        document.getElementById('input2').focus();
         input1.disabled = true
     }
 })
@@ -39,8 +38,7 @@ addEventListener('keyup', (e) => {
         input2.value = "Are"
         input2.style.color = 'green'
         input2.style.backgroundColor = 'yellow'
-        document.getElementById('input3').focus()
-        document.getElementById('click').play()
+        document.getElementById('input3').focus();
         input2.disabled = true
     }
 })
@@ -51,8 +49,7 @@ addEventListener('keyup', (e) => {
         input3.value = "are"
         input3.style.color = 'darkgreen'
         input3.style.backgroundColor = 'yellow'
-        document.getElementById('input4').focus()
-        document.getElementById('click').play()
+        document.getElementById('input4').focus();
         input3.disabled = true
     }
 })
@@ -62,8 +59,7 @@ addEventListener('keyup', (e) => {
         correct4.style.visibility = 'visible'
         input4.style.color = 'darkgreen'
         input4.style.backgroundColor = 'yellow'
-        document.getElementById('input5').focus()
-        document.getElementById('click').play()
+        document.getElementById('input5').focus();
         input4.disabled = true
     }
 })
@@ -74,8 +70,7 @@ addEventListener('keyup', (e) => {
         correct5.style.visibility = 'visible'
         input5.style.color = 'darkgreen'
         input5.style.backgroundColor = 'yellow'
-        document.getElementById('input6').focus()
-        document.getElementById('click').play()
+        document.getElementById('input6').focus();
         input5.disabled = true
     }
 })
@@ -85,8 +80,7 @@ addEventListener('keyup', (e) => {
         correct6.style.visibility = 'visible'
         input6.style.color = 'green'
         input6.style.backgroundColor = 'yellow'
-        document.getElementById('input7').focus()
-        document.getElementById('click').play()
+        document.getElementById('input7').focus();
         input6.disabled = true
     }
 })
@@ -97,8 +91,7 @@ addEventListener('keyup', (e) => {
         correct7.style.visibility = 'visible'
         input7.style.color = 'green'
         input7.style.backgroundColor = 'yellow'
-        document.getElementById('input8').focus()
-        document.getElementById('click').play()
+        document.getElementById('input8').focus();
         input7.disabled = true
     }
 })
@@ -109,7 +102,6 @@ addEventListener('keyup', (e) => {
         correct8.style.visibility = 'visible'
         input8.style.color = 'green'
         input8.style.backgroundColor = 'yellow'
-        document.getElementById('click').play()
         input8.disabled = true
     }
 })
